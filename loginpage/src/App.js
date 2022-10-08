2f63889d5291d2068d607a60ea675ee90e5fe403
shweta <shwetakodamg@gmail.com>
2022-10-08 12:34:29 +0530

    login

---
+++ loginpage/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './components/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
